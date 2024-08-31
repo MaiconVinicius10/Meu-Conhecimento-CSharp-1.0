6cc9aa6ceff4413cdb03618226582f46aea534aa
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-08-31 17:51:12 -0300

    F_Menu - Adicionando Recursos:
    1-)Botão Adicionar funcionando.
    2-)Botão Limpar Funcionando.

--- Forms/F_Menu.cs
+++ Forms/F_Menu.cs
@@ -16,5 +16,18 @@ namespace Meu_Conhecimento_CSharp_1._0
         {
             InitializeComponent();
         }
+        //Botão Adicionar
+        private void btn_Adicionar_Click(object sender, EventArgs e)
+        {   //recebendo o valor digitado
+            txt_ListaVeiculos.Text = txt_AdicionarVeiculo.Text;
+        }
+        //Botão Limpar
+        private void btb_Limpar_Click(object sender, EventArgs e)
+        {   //apagando 
+            txt_AdicionarVeiculo.Clear();
+            txt_ListaVeiculos.Clear();
+            //setando meu ponteio no adicionar
+            txt_AdicionarVeiculo.Focus();
+        }
     }
 }
