536c7b05b36994834378a08f917a77e3fdfa6dbe
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-09-07 08:13:24 -0300

    Adicionado MenuStrip - para Controle de Telas Adicionado - F_CheckBox e Editiado o Designer

---
+++ Forms/F_CheckedListBox.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Meu_Conhecimento_CSharp_1._0.Forms
+{
+    partial class F_CheckedListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // F_CheckedListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 536);
+            this.Name = "F_CheckedListBox";
+            this.Text = "F_CheckedListBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
