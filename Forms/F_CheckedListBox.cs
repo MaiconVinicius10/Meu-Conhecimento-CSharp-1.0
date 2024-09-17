6abd0adf223983ad43d032155c76c0e64a3fb835
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-09-17 18:12:46 -0300

    F_CheckedListBox - Adicionando Funcionalidade nos Botões 1-)Mostrar Selecionados 2-)Limpar Lista 3-)Resetar Lista 4-)Adicionar Novo Transporte

--- Forms/F_CheckedListBox.cs
+++ Forms/F_CheckedListBox.cs
@@ -11,10 +11,49 @@ using System.Windows.Forms;
 namespace Meu_Conhecimento_CSharp_1._0.Forms
 {
     public partial class F_CheckedListBox :Form
-    {
+    {   // Criando lista veiculos
+        List<String> transp = new List<String>();
         public F_CheckedListBox()
         {
             InitializeComponent();
+            transp.Add("Carro");
+            transp.Add("Avião");
+            transp.Add("Navio");
+            transp.Add("Ônibus");
+            transp.Add("Trem");
+        }
+        // botão Mostrar Selecionados
+        private void btn_MostrarSelecionados_Click(object sender, EventArgs e)
+        {
+            String txt = "";
+            
+            foreach(String t in cb_Transportes.CheckedItems)
+            {
+                txt += t+", ";
+            }
+
+            MessageBox.Show(txt.ToString());
+        }
+        // botão Limpar Lista
+        private void btn_LimparLista_Click(object sender, EventArgs e)
+        {
+            cb_Transportes.Items.Clear();
+            txt_NovoTransporte.Clear();
+            txt_NovoTransporte.Focus();
+        }
+        // botão Resetar Lista
+        private void btn_ResetarLista_Click(object sender, EventArgs e)
+        {
+            cb_Transportes.Items.Clear();
+            cb_Transportes.Items.AddRange(transp.ToArray());
+            txt_NovoTransporte.Focus();
+        }
+        //Botão Adicionar Novo Transporte
+        private void btn_AdicionarNovoTransporte_Click(object sender, EventArgs e)
+        {
+            cb_Transportes.Items.Add(txt_NovoTransporte.Text);
+            txt_NovoTransporte.Clear();
+            txt_NovoTransporte.Focus();
         }
     }
 }
