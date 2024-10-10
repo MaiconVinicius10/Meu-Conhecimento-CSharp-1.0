80957717b0cda35038c54243b28783fc17c72c2f
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2024-10-10 19:47:24 -0300

    F_ListView Editado

---
+++ Forms/F_ListView.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Meu_Conhecimento_CSharp_1._0.Forms
+{
+    partial class F_ListView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "0",
+            "Mouse",
+            "120",
+            "19.99"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "1",
+            "Teclado",
+            "85",
+            "39.99"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "2",
+            "Monitor",
+            "38",
+            "500.00"}, -1);
+            this.lv_Produtos = new System.Windows.Forms.ListView();
+            this.col_id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Produto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Quantidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_Preco = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lv_Produtos
+            // 
+            this.lv_Produtos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_id,
+            this.col_Produto,
+            this.col_Quantidade,
+            this.col_Preco});
+            this.lv_Produtos.FullRowSelect = true;
+            this.lv_Produtos.HideSelection = false;
+            this.lv_Produtos.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.lv_Produtos.Location = new System.Drawing.Point(12, 12);
+            this.lv_Produtos.MultiSelect = false;
+            this.lv_Produtos.Name = "lv_Produtos";
+            this.lv_Produtos.Size = new System.Drawing.Size(491, 102);
+            this.lv_Produtos.TabIndex = 0;
+            this.lv_Produtos.UseCompatibleStateImageBehavior = false;
+            this.lv_Produtos.View = System.Windows.Forms.View.Details;
+            // 
+            // col_id
+            // 
+            this.col_id.Text = "ID";
+            this.col_id.Width = 40;
+            // 
+            // col_Produto
+            // 
+            this.col_Produto.Text = "Produto";
+            this.col_Produto.Width = 250;
+            // 
+            // col_Quantidade
+            // 
+            this.col_Quantidade.Text = "Quantidade";
+            this.col_Quantidade.Width = 80;
+            // 
+            // col_Preco
+            // 
+            this.col_Preco.Text = "Preço";
+            this.col_Preco.Width = 80;
+            // 
+            // F_ListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 380);
+            this.Controls.Add(this.lv_Produtos);
+            this.Name = "F_ListView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "F_ListView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_Produtos;
+        private System.Windows.Forms.ColumnHeader col_id;
+        private System.Windows.Forms.ColumnHeader col_Produto;
+        private System.Windows.Forms.ColumnHeader col_Quantidade;
+        private System.Windows.Forms.ColumnHeader col_Preco;
+    }
+}
